d1d58e2f3e6421b3e956ccdeff4e858640110e0e
superryanguo <superryanguo@gmail.com>
2019-07-24 21:14:20 +0800

    try the parse gpb

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"os/exec"
 
 	"gpbdecoder/goproto/proto"
 
@@ -22,10 +23,29 @@ func main() {
 	}
 	fmt.Printf("Before Marshal data:%v\n", u)
 	fmt.Printf("After Marshal data:%x\n", data)
+
 	ur := &myobject.User{}
 	err = proto.Unmarshal(data, ur)
 	fmt.Printf("After Unmarshal data:%v\n", ur)
 	if err != nil {
 		log.Fatal("unmarshaling error: ", err)
 	}
+
+	//check the exec runtime result
+	cmdstr := fmt.Sprintf("echo %x | xxd -r -p | protoc --decode_raw", data)
+	output := runshell(cmdstr)
+	fmt.Printf("the shell cmd output is:\n%s\n", output)
+	message := fmt.Sprintf("%T", u)
+	fmt.Println("fmt type:", message)
+	cmdstr = fmt.Sprintf("echo %x | xxd -r -p | protoc --decode %s proto/myobject.proto", data, message)
+	output = runshell(cmdstr)
+	fmt.Printf("the shell cmd output is:\n%s\n", output)
+}
+func runshell(shell string) []byte {
+	cmd := exec.Command("sh", "-c", shell)
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		log.Fatal(err)
+	}
+	return output
 }
